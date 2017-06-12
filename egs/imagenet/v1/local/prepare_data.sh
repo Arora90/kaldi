ac792b6a96d93819e82d5cf4d1f016861179d028
Ashish Arora <aaror@b18.clsp.jhu.edu>
2017-06-12 18:49:36 -0400

    updating process data, calling cropping routine for test images

--- egs/imagenet/v1/local/prepare_data.sh
+++ egs/imagenet/v1/local/prepare_data.sh
@@ -112,7 +112,7 @@ echo 3 > data/train_t12/num_channels
 echo 3 > data/train_t3/num_channels
 echo 3 > data/test/num_channels
 
-local/process_data.py $val_data | \
+local/process_test_data.py $val_data True| \
   copy-feats --compress=true --compression-method=7 \
     ark:- ark,scp:data/test/data/images.ark,data/test/images.scp || exit 1
 
